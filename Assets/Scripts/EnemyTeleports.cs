95aab3abe2a9ead9ac3031796150304c518709d0
Nekot94 <nekomato9@gmail.com>
2017-02-03 10:11:56 +0300

    first cave

---
+++ Assets/Scripts/EnemyTeleports.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTeleports : MonoBehaviour {
+
+    [SerializeField] GameObject background;
+
+
+    public float minTime = 2f;
+    public float maxTime = 5f;
+
+    private float offset;
+    private float x, y;
+    private float maxX, maxY;
+
+    // Use this for initialization
+    void Start () {
+        SpriteRenderer backgroundSprite = background.GetComponent<SpriteRenderer>();
+        BoxCollider2D backgroundCollider = background.GetComponent<BoxCollider2D>();
+        CircleCollider2D spawnCollider = GetComponent<CircleCollider2D>();
+        offset = Mathf.Min(backgroundCollider.size.x, backgroundCollider.size.y) + spawnCollider.radius;
+
+        maxX = backgroundSprite.sprite.bounds.size.x / 2 - offset;
+        maxY = backgroundSprite.sprite.bounds.size.y / 2 - offset;
+        Debug.Log(maxX);
+
+        StartCoroutine(Teleport());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+   IEnumerator Teleport() {
+        float seconds;
+        while (true) {
+            seconds = Random.Range(minTime, maxTime);
+            x = Random.Range(-maxX, maxX);
+            y = Random.Range(-maxY, maxY);
+            transform.position = new Vector3(x, y, 0);
+            transform.localScale += new Vector3(0.3f, 0.3f, 0.3f);
+            yield return new WaitForSeconds(0.2f);
+            transform.localScale -= new Vector3(0.3f, 0.3f, 0.3f);
+            yield return new WaitForSeconds(seconds);
+        }
+
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 1.0f;
+    public Text countText;
+    public Text winText;
+    public int winCount = 10;
+
+
+    private Rigidbody2D rb2d;
+    private int count;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        winText.text = "";
+        countText.text = "Очки: " + count.ToString() + "/" + winCount.ToString();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal,moveVertical);
+        rb2d.AddForce(movement * speed);
+        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("PickUp")) {
+            collision.gameObject.SetActive(false);
+            count += 1;
+            countText.text = "Очки: " + count.ToString() + "/" + winCount.ToString();
+        }
+        if (count >= winCount) {
+            winText.text = "Ты победил";
+            StartCoroutine(NextScene(3f));
+            GetComponent<CircleCollider2D>().enabled = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        if (coll.gameObject.CompareTag("Enemy")) {
+            winText.text = "Ты проиграл";
+            winText.color = Color.red;
+            StartCoroutine(Restart(3f));
+            GetComponent<CircleCollider2D>().enabled = false;
+        }
+    }
+
+    IEnumerator NextScene(float time) {
+        yield return new WaitForSeconds(time);
+        string sceneName = SceneManager.GetActiveScene().name;
+        string nextSceneName = "Scene" + ((int)(sceneName[sceneName.Length - 1] - '0') + 1).ToString();
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+
+    IEnumerator Restart(float time) {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
